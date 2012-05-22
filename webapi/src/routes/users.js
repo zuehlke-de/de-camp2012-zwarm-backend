6e15cb114da6624f3bc0c76350700a1ba1dfb14c
tor <tor@zuehlke.com>
2012-05-22 18:27:31 +0200

    Added POST /users

--- webapi/src/routes/users.js
+++ webapi/src/routes/users.js
@@ -1,3 +1,5 @@
+var n = require('nimble');
+
 // radius for nearby users in meters
 var default_radius = 500,
     minimum_radius = 1;
@@ -6,7 +8,54 @@ var default_radius = 500,
  * Create new user
  */
 exports.create = function (req, res) {
-    res.send(201);
+
+    var user = req.body,
+        requiredFields = ['id', 'nickname', 'platform'],
+        isValid = true;
+
+    // check if JSON
+    if (!req.is('json')) {
+        res.send('Requests must be in JSON format!', 400);
+        return;
+    }
+
+    // check required fields
+    n.each(requiredFields, function (f) {
+        isValid = isValid && user[f] !== undefined;
+    });
+    if (!isValid) {
+        res.send("One of the required fields is not set: " + requiredFields.join(", "), 400);
+        return;
+    }
+
+    // check for semantic correctness
+    if (user.id == "") {
+        res.send("id must not be empty!", 400);
+        return;
+    }
+    if (user.nickname == "") {
+        res.send("nickname must not be empty!", 400);
+        return;
+    }
+    if (!(user.platform === "ios" || user.platform === "android")) {
+        res.send("platform must be one of ios or android!", 400);
+        return;
+    }
+
+    // store user in database
+    db.save(user.id, user, function (err) {
+
+        if (err) {
+            console.log("Create user: could not store user: %s\n-> %s", JSON.stringify(err), JSON.stringify(user));
+            res.send("Could not store user!", 500);
+            return;
+        }
+
+        // signal creation
+        delete user._rev;
+        res.json(user, 201);
+        console.log("Create user: User created: %s (%s)", user.id, user.nickname);
+    });
 };
 
 /**
